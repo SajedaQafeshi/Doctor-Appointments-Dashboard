63d33ee4a65514d61d819e6c802e67a3e47be7a5
ssaajjiiddaa <ssaajjiiddaa@gmail.com>
2020-02-23 00:42:22 +0200

    Login Commit

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ let MongoClient = require('mongodb').MongoClient;
 let express = require('express');
 let path = require('path');
 let app = express();
+let bodyParser = require('body-parser');
 let url = "mongodb://localhost:27017/Doctor-Appointments-Dashboard";
 let exphbs = require('express-handlebars');
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.set('view engine', '.hbs');
 app.engine('.hbs' , exphbs({extname : '.hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/'}));
@@ -19,6 +22,33 @@ app.listen(5000,  () => {
 //root path 
 
 app.get('/',  (req, res) => {
-    res.render('home.hbs',{titel:"Home"});
+    res.render('home.hbs',{
+        titel:"Home",
+        style:'home.css'
+    });
 });
+
+app.get('/login',  (req, res) => {
+    res.render('login.hbs',{
+        titel:"Login",
+        style:'login.css'
+    });
+});
+
+app.post('/login',  (req, res) => {
+    console.log(req.body);
+    res.render('login.hbs',{
+        titel:"Login",
+        style:'login.css'
+    });
+});
+
+app.get('/',  (req, res) => {
+    res.render('home.hbs',{
+        titel:"Home",
+        style:'home.css'
+    });
+});
+
+
 
